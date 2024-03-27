16437801314df5e2352bce75ef312ea551066891
Manuel Beracah <manuel.b@kalvium.community>
2024-03-27 16:01:24 +0530

    linked pzges

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "../src/component//Navbar";
+import Navbar from "../src/component/Navbar";
 import Home from "../src/component/Home"
 import Login from "../src/component/Login"
 import Signup from './component/Signup';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Navbar" element={<Navbar/>} />
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/Signup" element={<Signup/>}/>
+        <Route path="/Navbar" element={<Navbar />} />
+        <Route
+          path="/Login"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        />
+        <Route
+          path="/Signup"
+          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+        />
       </Routes>
     </Router>
   );
