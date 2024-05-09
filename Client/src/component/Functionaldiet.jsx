41a8fb09dbccbba7504819239e843e4bf50398f0
Manuel Beracah <manuel.b@kalvium.community>
2024-05-09 17:14:31 +0530

    Functional Training

--- Client/src/component/Functionaldiet.jsx
+++ Client/src/component/Functionaldiet.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Functionaldiet() {
   return (
-    <div>Functionaldiet</div>
-  )
+    <div>
+      <p>Welcome to Functional Training Diet, your ultimate destination for achieving your fitness goals with precision and expertise.</p>
+      <div className='food'>
+        <Link to="/Funcveg">
+          <button>VEG</button>
+        </Link>
+        <Link to="/Funcnonveg">
+          <button>NON-VEG</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Functionaldiet
+export default Functionaldiet;
+
