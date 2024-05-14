3d02756163ef24737d1bed1b9d677c1273f0a9fc
Manuel Beracah <manuel.b@kalvium.community>
2024-05-14 17:56:42 +0530

    login signup done

--- Client/src/component/Login.jsx
+++ Client/src/component/Login.jsx
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types'; // Import PropTypes
 import { useNavigate, Link } from 'react-router-dom';
+import axios from "axios";
 import "./Login.css";
 
 const Login = ({ setIsLoggedIn }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-
-  const handleLogin = () => {
-    if (username === 'manuel' && password === '123456789') {
-      setIsLoggedIn(true);
-      navigate('/');
-    } else {
-      alert('Invalid username or password');
-    }
+  function getCookie(name) {
+    let cookieArray = document.cookie.split('; ');
+    let cookie = cookieArray.find((row) => row.startsWith(name + '='));
+    return cookie ? cookie.split('=')[1] : null;
+  }
+  function setCookie(name, value, daysToExpire) {
+    let date = new Date();
+    date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+  }
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3000/login',{
+                name:username,
+                password:password
+            }).then((response)=>{
+            setCookie('token', response.data.accessToken,365);
+            setCookie('username', username,365);
+        navigate('/')}).catch((error)=>{console.error(error)});
   };
 
   return (
     <div className="login-page">
     <div className="login-container">
       <h2>Login</h2>
-      <form>
+      <form onSubmit={handleLogin}>
         <div className="form-group">
           <label>Username:</label>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -30,7 +42,7 @@ const Login = ({ setIsLoggedIn }) => {
           <label>Password:</label>
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <button type="button" onClick={handleLogin} id='login'>Login</button>
+        <button type="submit" id='login'>Login</button>
       </form>
       <p>Don't have an account? <Link to="/signup">Create account</Link></p>
     </div>
--- Client/src/component/Signup.jsx
+++ Client/src/component/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
 import './Signup.css';
 
 const Signup = ({ setIsLoggedIn }) => {
@@ -8,25 +9,32 @@ const Signup = ({ setIsLoggedIn }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-
-  const handleSignup = () => {
-    // Implement your signup logic here (e.g., sending form data to the server)
-    console.log('Signing up...');
-    console.log('Username:', username);
-    console.log('Email:', email);
-    console.log('Password:', password);
-    // Reset form fields after signup
-    setUsername('');
-    setEmail('');
-    setPassword('');
-    setIsLoggedIn(true);
-    navigate('/');
+  function getCookie(name) {
+    let cookieArray = document.cookie.split('; ');
+    let cookie = cookieArray.find((row) => row.startsWith(name + '='));
+    return cookie ? cookie.split('=')[1] : null;
+}
+function setCookie(name, value, daysToExpire) {
+    let date = new Date();
+    date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}
+  const handleSignup = (e) => {
+    e.preventDefault();
+            axios.post('http://localhost:3000/signup',{
+                name:username,
+                email:email,
+                password:password
+            }).then((response)=>{console.log(response);
+                setCookie('token', response.data.accessToken,365)
+                setCookie('username',username,365);
+        navigate('/')}).catch((error)=>{console.error(error)});
   };
 
   return (
     <div className="signup-container">
       <h2>Sign Up</h2>
-      <form>
+      <form onSubmit={handleSignup}>
         <div className="form-group">
           <label>Username:</label>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -39,7 +47,7 @@ const Signup = ({ setIsLoggedIn }) => {
           <label>Password:</label>
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
-        <button type="button" onClick={handleSignup}>Sign Up</button>
+        <button type="submit">Sign Up</button>
       </form>
       <p>
         Already have an account? <Link to="/login">Login</Link>
