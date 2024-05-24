5827d2beef5c05a4d4ab7893f278a6fcac160d51
Manuel Beracah <manuel.b@kalvium.community>
2024-05-24 16:38:42 +0530

    Changes

--- Client/src/component/Gymgoals.jsx
+++ Client/src/component/Gymgoals.jsx
@@ -4,7 +4,11 @@ import './gymgoals.css';
 import strength from "../assets/strength.png";
 import muslebuilding from "../assets/muslebuilding.png";
 import fatloss from "../assets/fat loss.png";
-import Endurance from "../assets/Endurance training.jpg";
+import Endurance from "../assets/Endurance training.png";
+import flexibility from "../assets/flexibility and mobility.png";
+import Functional_Training from "../assets/Funtional_training.png";
+import sports_specific from "../assets/sports specific training.png";
+import abs from "../assets/abs workout.png";
 
 const GymGoals = () => {
   return (
@@ -36,21 +40,25 @@ const GymGoals = () => {
       </div>
       <div className="box">
       <Link to ="/Flexibility">
+        <img src={flexibility} alt="" />
         <p>Flexibility and Mobility</p>
         </Link>
       </div>
       <div className="box">
       <Link to="/Functional">
+        <img src={Functional_Training} alt="" />
         <p>Functional Training</p>
       </Link>
       </div>
       <div className="box">
       <Link to="/Sport">
+        <img src={sports_specific} alt="" />
         <p>Sport-Specific Training</p>
       </Link>
       </div>
       <div className="box">
       <Link to="/Abs">
+        <img src={abs} alt="" />
         <p>ABS</p>
       </Link>
       </div>
